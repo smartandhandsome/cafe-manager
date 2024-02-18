4ad861dbc80fa372c7b6cb451a904a85708eef46
sangminnim <sangmins930@gmail.com>
2024-02-19 06:19:23 +0900

    chore: Swagger 적용

--- src/main/java/com/cafe/product/presentation/request/ProductRegistrationRequest.java
+++ src/main/java/com/cafe/product/presentation/request/ProductRegistrationRequest.java
@@ -2,6 +2,7 @@ package com.cafe.product.presentation.request;
 
 import com.cafe.product.service.vo.ProductInfoRegistrationForm;
 import com.cafe.product.service.vo.SizeRegistrationForm;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,21 +11,30 @@ import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
+@Schema(description = "상품 등록 요청 객체")
 public record ProductRegistrationRequest(
+        @Schema(description = "카테고리 id")
         @NotNull
         Long productCategoryId,
+        @Schema(description = "기본 가격")
         @PositiveOrZero
         int basePrice,
+        @Schema(description = "기본 원가")
         @PositiveOrZero
         int baseCost,
+        @Schema(description = "이름")
         @NotBlank
         String name,
+        @Schema(description = "설명")
         @NotBlank
         String description,
+        @Schema(description = "바코드")
         @NotBlank
         String barcode,
+        @Schema(description = "유통기한")
         @NotNull
         String expirationDuration,
+        @Schema(description = "사이즈 등록 요청 객체 목록")
         @Valid
         @Size(min = 1)
         @NotNull
--- src/main/java/com/cafe/product/presentation/request/SizeRegistrationRequest.java
+++ src/main/java/com/cafe/product/presentation/request/SizeRegistrationRequest.java
@@ -1,14 +1,19 @@
 package com.cafe.product.presentation.request;
 
 import com.cafe.product.service.vo.SizeRegistrationForm;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 
+@Schema(description = "사이즈 등록 요청 객체")
 public record SizeRegistrationRequest(
+        @Schema(description = "사이즈 이름")
         @NotBlank
         String name,
+        @Schema(description = "추가 금액")
         @PositiveOrZero
         int extraCharge,
+        @Schema(description = "추가 원가")
         @PositiveOrZero
         int extraCost
 ) {
